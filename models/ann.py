884f91902d3c652e9821cc7ee9b57d92825f996b
zarubinKV <70758674+zarubinKV@users.noreply.github.com>
2022-12-11 22:52:25 +0300

    Add hw4

---
+++ models/ann.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class Ann():
+    """Class for fit-perdict Ann model"""
+
+    def __init__(self, label: list, distance: list):
+        self.label = label
+        self.distance = distance
+
+    def predict(self, user_id: int):
+        return self.label[user_id]
